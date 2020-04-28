bc45ba3df8f2143d63ee635846c98ad2ae01cc39
haritaanchal21 <49859121+haritaanchal21@users.noreply.github.com>
2020-04-28 17:18:38 +0530

    Add files via upload
    First javascript trial website(choose me).

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1=Math.floor(Math.random()*6)+1
+var randomDiceImage1="images/dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1);
+
+var randomNumber2=Math.floor(Math.random()*6)+1
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🙌Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins🙌";
+}
+else{
+  document.querySelector("h1").innerHTML="😜Draw😜";
+}
